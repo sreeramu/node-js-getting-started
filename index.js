99d375c3e7a5065dc8703c06f48c9cb45b3ea569
Sreenivasan K <sreeramu@gmail.com>
2018-06-12 18:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ express()
         });
         req.on('end', function () {
             console.log("Body: " + body);
+          res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received' + body);
         });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
